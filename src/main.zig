db5a06daf6860984ba7060e76831404c4b7a47ee
SpandanBG <spandanbg@gmail.com>
2024-07-04 00:30:11 +0530

    add logic to build char count pair array list

--- src/main.zig
+++ src/main.zig
@@ -41,10 +41,75 @@ fn encode() !void {
 
     var out = std.io.getStdOut();
     defer out.close();
+
+    var data = try read_in(in);
+    defer data.deinit();
+
+    try build_huffman_tree(data);
 }
 
 fn decode() !void {}
 
+const size_pair = struct {
+    size: u64,
+    char: u8,
+
+    const Self = @This();
+
+    fn lt(_: void, a: Self, b: Self) bool {
+        return a.size < b.size;
+    }
+};
+
+fn build_huffman_tree(data: std.ArrayList(u8)) !void {
+    const allocator = std.heap.page_allocator;
+
+    // ------------ count chars
+    var all_ascii = std.mem.zeroes([128]u64);
+    var unique_chars: usize = 0;
+
+    for (data.items) |ch| {
+        const char = @as(usize, ch);
+        if (all_ascii[char] == 0) unique_chars += 1;
+        all_ascii[char] += 1;
+    }
+    // -------------------------
+
+    // ------------ create sorted count-char pairs
+    var pairs = try allocator.alloc(size_pair, unique_chars);
+    defer allocator.free(pairs);
+
+    var pi: usize = 0;
+    for (all_ascii, 0..) |size, i| {
+        if (size == 0) continue;
+        const ch: u8 = @intCast(i);
+        pairs[pi] = .{ .size = size, .char = ch };
+        pi += 1;
+    }
+
+    std.sort.insertion(size_pair, pairs[0..], {}, size_pair.lt);
+    // ----------------------------
+}
+
+fn read_in(in: std.fs.File) !std.ArrayList(u8) {
+    var data = std.ArrayList(u8).init(std.heap.page_allocator);
+    var char_buff: [1:0]u8 = undefined;
+
+    while (true) {
+        const read = try in.read(&char_buff);
+        if (read == 0) break;
+        try data.append(char_buff[0]);
+    }
+
+    return data;
+}
+
 fn invalid_cmd() void {
-    std.log.err("invalid arg\nusage: (-e|-d)\n\t-e\tencode input\n\t-d\tdecode input", .{});
+    std.log.err(
+        \\invalid arg
+        \\
+        \\usage: <input> | zig build run -- (-e|-d) > <output>
+        \\      -e      encode
+        \\      -d      decode
+    , .{});
 }
